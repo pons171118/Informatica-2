40dc1148bd93d24a36146c7ed2aced08b815af93
pons171118 <32199943+pons171118@users.noreply.github.com>
2018-03-03 21:41:45 -0600

    Hoja 5 subida
    No logre subirla por git bash, asi que le di upload en el repositorio, los paquetes no pudieron ser incluidos.

---
+++ HT5/Hoja5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HT5
+
+{
+    public class Program
+    {
+        public static int ReduceInt(int [] vlr, int acc, Func<int,int,int> reduccion)
+        {
+            for (int i = 0; i < vlr.Length; i++)
+            {
+                acc = reduccion(acc, vlr[i]);
+            }
+
+            return acc;
+        }
+
+        public static A Reduce<T, A>(T[] valores, A acc, Func<A, T, A> reduccion)
+        {
+            for (int i = 0; i < valores.Length; i++)
+            {
+                acc = reduccion(acc, valores[i]);
+            }
+
+            return acc;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ HT5/ReduceTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hoja5;
+
+namespace ReduceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestReduceN()
+        {
+            int[] valores = new int[] { 2, 4, 5, 9 };
+            Assert.AreEqual(Program.ReduceInt(valores, 0, (acumulador, valor) => acumulador + valor), 20);
+        }
+
+        [TestMethod]
+        public void TestReduceG()
+        {
+            string[] valores = new string[] { "Que", "onda", "vos" };
+            Assert.AreEqual(Program.Reduce<string, int>(valores, 0, (acumulador, valor) => acumulador + valor.Length), 13);
+        }
+    }
+}
