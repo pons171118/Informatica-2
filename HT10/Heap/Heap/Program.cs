4e66546c0e7680d6ae40666985696d919ab7bed1
pons171118 <32199943+pons171118@users.noreply.github.com>
2018-05-13 22:43:47 -0600

    Hoja de trabajo 10

---
+++ HT10/Heap/Heap/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heap
+{
+	public class Program
+	{
+		public static int Heap;
+			
+		static void Main(string[] args)
+		{
+		}
+
+		public static int[] HeapSort(int[] arr) {
+			int n = arr.Length;
+
+			
+			for (int i = n / 2 - 1; i >= 0; i--)
+				ShiftDown(arr, n, i);
+
+			for (int i = n - 1; i >= 0; i--)
+			{
+				int temp = arr[0];
+				arr[0] = arr[i];
+				arr[i] = temp;
+
+				ShiftDown(arr, i, 0);
+			}
+			return arr;
+		}
+
+		public static void ShiftDown(int[] arr, int n, int i)
+		{
+
+			int largest = i;
+			int l = 2 * i + 1;
+			int r = 2 * i + 2;
+
+			if (l < n && arr[l] > arr[largest])
+				largest = l;
+
+			if (r < n && arr[r] > arr[largest])
+				largest = r;
+
+			if (largest != i)
+			{
+				int swap = arr[i];
+				arr[i] = arr[largest];
+				arr[largest] = swap;
+
+				ShiftDown(arr, n, largest);
+			}
+
+		}
+
+		private static int floop(int currentIndex)
+		{
+			return (currentIndex - 1)/ 2;
+		}
+
+		private static int Izquierdo(int currentIndex)
+		{
+			return currentIndex * 2 + 2;
+		}
+
+		private static int Derecho(int currentIndex)
+		{
+			return currentIndex * 2 + 1;
+		}
+
+	}
+}
---
+++ HT10/Heap/HeapTest/HeapTest.cs
@@ -0,0 +1,18 @@
+﻿using Heap;
+using System;
+using Xunit;
+
+namespace HeapTest
+{
+	public class HeapTest
+	{
+		[Fact]
+		public void HeapSort() {
+			int[] arr = new int[] { 30,2,0,12,5};
+
+			Program.HeapSort(arr);
+
+			Assert.Equal(new int[] { 0,2,5,12,30}, arr);
+		}
+	}
+}
